428bde8db22d57d2b507ab2bf6a19eeab1c700d8
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:04:16 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+<script>
+        for (let i=1; i<129; i*=2) {
+            console.log("i= " +i)
+        }
+    console.log("Bye")
+    for (var i=1; i<20; i++) {
+        console.log(i)
+    }
+
+    </script>    
